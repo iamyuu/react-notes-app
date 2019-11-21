04964a0af590a4647a7422563e29257f8f8d87d7
iamyuu <akuyusuf27@gmail.com>
2019-11-21 11:18:36 +0700

    add css styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./screens/app-with-hooks";
+import "./styles.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
