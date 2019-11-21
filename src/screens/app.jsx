8671a9c6d19279efe7875bc47157f261605b4a03
iamyuu <akuyusuf27@gmail.com>
2019-11-21 11:16:01 +0700

    add class components version

--- src/screens/app.jsx
+++ src/screens/app.jsx
@@ -1,23 +1,111 @@
 import React from "react";
+import Card from "../components/card";
+import Form from "../components/form";
+import initialState from "./items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  state = {
+    mode: "create",
+    items: initialState,
+    formItem: {
+      title: "",
+      note: ""
+    }
+  };
+
+  handleInputChange(name, value) {
+    const { formItem } = this.state;
+    const newFormitem = { ...formItem, [name]: value };
+
+    this.setState({ formItem: newFormitem });
+  }
+
+  handleCreate() {
+    const { items, formItem } = this.state;
+    const { title, note } = formItem;
+
+    const newItems = {
+      id: items.length + 1,
+      title,
+      note
+    };
+
+    this.setState({
+      items: [...items, newItems],
+      formItem: { title: "", note: "" }
+    });
+  }
+
+  handleUpdate() {
+    const { items, formItem } = this.state;
+    const index = items.findIndex(item => item.id === formItem.id);
+
+    const updatedItems = [...items];
+    updatedItems[index] = formItem;
+
+    this.setState({
+      mode: "create",
+      items: updatedItems,
+      formItem: { title: "", note: "" }
+    });
+  }
+
+  handleEdit(index) {
+    const { items } = this.state;
+
+    this.setState({
+      mode: "edit",
+      formItem: items[index]
+    });
+  }
+
+  handleDelete(id) {
+    const { items } = this.state;
+    const newItems = items.filter(item => item.id !== id);
+
+    this.setState({
+      items: newItems,
+      formItem: { title: "", note: "" }
+    });
+  }
+
+  render() {
+    const { mode, items, formItem } = this.state;
+
+    return (
+      <>
+        <Form
+          mode={mode}
+          item={formItem}
+          onCreate={this.handleCreate}
+          onUpdate={this.handleUpdate}
+          onInputChange={this.handleInputChange}
+        />
+
+        <br />
+
+        {items.length ? (
+          <Card
+            items={items}
+            onEdit={this.handleEdit}
+            onDelete={this.handleDelete}
+          />
+        ) : (
+          <p className="text-center">Tidak ada data</p>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
